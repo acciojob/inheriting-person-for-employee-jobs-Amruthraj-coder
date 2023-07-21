1290a426ce1e4b3642dd39a56ca433baca8de3f1
Amruthraj-coder <122180037+Amruthraj-coder@users.noreply.github.com>
2023-07-22 00:03:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function Employee(name, age, jobTitle) {}
 Employee.prototype.__proto__=person.prototype;
 
 Employee.prototype.jobGreet=function(){
-    console.log(`Hello, my name is ${}, I am ${} years old, and my job title is ${}.`)
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 }
 
 // Do not change code below this line
