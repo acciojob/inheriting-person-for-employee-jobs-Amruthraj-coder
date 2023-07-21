367d28d802bb3fb0a7c70449123bf35cd66ffe98
Amruthraj-coder <122180037+Amruthraj-coder@users.noreply.github.com>
2023-07-22 00:46:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function Employee(name, age, jobTitle) {
   this.jobTitle = jobTitle;
 }
 
-Employee.prototype.__proto__=person.prototype;
+Employee.prototype.__proto__=Person.prototype;
 
 Employee.prototype.jobGreet=function(){
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
