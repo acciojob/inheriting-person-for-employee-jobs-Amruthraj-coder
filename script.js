823d8a19bde0499ddf691e1aa49a5492242404bf
Amruthraj-coder <122180037+Amruthraj-coder@users.noreply.github.com>
2023-07-22 00:02:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	
+}
+
+person.prototype.greet=function(){
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+}
 
 function Employee(name, age, jobTitle) {}
+
+Employee.prototype.__proto__=person.prototype;
+
+Employee.prototype.jobGreet=function(){
+    console.log(`Hello, my name is ${}, I am ${} years old, and my job title is ${}.`)
+}
 
 // Do not change code below this line
 window.Person = Person;
